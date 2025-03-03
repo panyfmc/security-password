f3f0ead301dff89e9d7e92cf624a7abadb8e4a01
panyfmc <paulavanessa010@gmail.com>
2025-03-02 22:43:54 -0300

    :tada: initial commit

---
+++ crack.py
@@ -0,0 +1,8 @@
+from string import ascii_letters      #biblioteca que me dá acesso a todas as letras do alfabeto
+
+for i in ascii_letters:
+    for j in ascii_letters:
+        for k in ascii_letters:
+            for l in ascii_letters:
+                print(i, j, k, l)
+                
