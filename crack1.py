9f87fc71f90629bfabcfa52db320e9bfef1cb39f
panyfmc <paulavanessa010@gmail.com>
2025-03-03 16:52:11 -0300

    loop to count the time to find the password with length 64⁴ (a-z, A-Z)

---
+++ crack1.py
@@ -0,0 +1,7 @@
+from string import ascii_letters        # 64 x 64 x 64 x 64 
+
+for i in ascii_letters:
+    for j in ascii_letters: 
+        for k in ascii_letters: 
+            for l in ascii_letters:
+                print(i, j, k, l)
