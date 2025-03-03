30de48456428ecda5390d114d6e4bd999c7ef171
panyfmc <paulavanessa010@gmail.com>
2025-03-03 16:58:11 -0300

    loop to count the time to find the password with length 94⁴ (a-z, A-Z, 0-9, symbols)

---
+++ crack2.py
@@ -0,0 +1,8 @@
+from string import ascii_letters, digits, punctuation      #biblioteca que me dá acesso a todas as letras a-z, A-Z, números 0-9, e símbolos (32). 94⁴
+
+for i in ascii_letters + digits + punctuation:          #verifica a 1° posição da senha
+    for j in ascii_letters + digits + punctuation:          #verifica a 2° posição da senha
+        for k in ascii_letters + digits + punctuation:          #verifica a 3° posição da senha
+            for l in ascii_letters + digits + punctuation:          #verifica a 4° posição da senha
+                print(i, j, k, l)
+                
