c9eaaf60ee571cba7735f4d94012ae72316c3efb
panyfmc <paulavanessa010@gmail.com>
2025-03-03 16:51:02 -0300

    :sparkles: feat: loop to count the time until you find the password with length 10⁴

---
+++ crack0.py
@@ -0,0 +1,7 @@
+from string import digits    # 10 x 10 x 10 x 10jn,nhbgvl,/.ç;
+
+for i in digits:
+    for j in digits: 
+        for k in digits: 
+            for l in digits:
+                print(i, j, k, l)
