86bec07ef29f081418c37bd5b19cc107ef320f00
panyfmc <paulavanessa010@gmail.com>
2025-03-02 23:07:58 -0300

    :sparkles: feat: numbers and symbols added to the verification of possible passwords with 4 digits

--- crack.py
+++ crack.py
@@ -1,8 +1,8 @@
-from string import ascii_letters      #biblioteca que me dá acesso a todas as letras do alfabeto
+from string import ascii_letters, digits, punctuation      #biblioteca que me dá acesso a todas as letras a-z, A-Z, números 0-9, e símbolos (32)
 
-for i in ascii_letters:
-    for j in ascii_letters:
-        for k in ascii_letters:
-            for l in ascii_letters:
+for i in ascii_letters + digits + punctuation:          #verifica a 1° posição da senha
+    for j in ascii_letters + digits + punctuation:          #verifica a 2° posição da senha
+        for k in ascii_letters + digits + punctuation:          #verifica a 3° posição da senha
+            for l in ascii_letters + digits + punctuation:          #verifica a 4° posição da senha
                 print(i, j, k, l)
                 
